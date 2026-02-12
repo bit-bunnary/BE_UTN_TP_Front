894a85cb1fa6f908b7603b997df7d6d0268ae81a
bit.bunnary <51nicoleberger@gmail.com>
2026-02-12 11:14:20 -0300

    mini error

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import { Route, Routes } from 'react-router'
 import LoginScreen from './Screens/LoginScreen/LoginScreen'
 import RegisterScreen from './Screens/RegisterScreen/RegisterScreen'
-import AuthContextProvider from './Context/AuthContext'
+
 import AuthMiddleware from './Middlewares/AuthMiddleware'
 import WorkspaceContextProvider from './Context/WorkspaceContext'
 import HomeScreen from './Screens/HomeScreen/HomeScreen'
 import CreateWorkspaceScreen from './Screens/CreateWorkspaceScreen/CreateWorkspaceScreen'
 import WorkspaceScreen from './Screens/WorkspaceScreen/WorkspaceScreen'
+import AuthContextProvider from './Context/AuthContext'
 
 function App() {
   return (
